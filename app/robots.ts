5662d1afa7c1d5b44922364bc0c361e4af322a38
Akash Parmar <akashparmar77116@gmail.com>
2026-07-22 12:02:55 +0530

    Fix robots and SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,20 +1,22 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: [
-            {
-                userAgent: "*",
-                allow: "/",
-                disallow: [
-                    "/api/",
-                    "/admin/",
-                    "/dashboard/",
-                    "/private/",
-                ],
-            },
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/admin/",
+          "/dashboard/",
+          "/private/",
         ],
+      },
+    ],
 
-        sitemap: "https://piperesque.com/sitemap.xml",
-    };
+    sitemap: "https://www.piperesque.com/sitemap.xml",
+
+    host: "https://www.piperesque.com",
+  };
 }
